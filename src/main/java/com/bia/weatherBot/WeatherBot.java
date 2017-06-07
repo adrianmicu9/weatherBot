6a49ac7489999b022825a2d0fd32ea0179b9cdcb
Bebita <Bebita@Bebita-PC>
2017-06-07 23:08:56 +0300

    Initial commit

---
+++ src/main/java/com/bia/weatherBot/WeatherBot.java
@@ -0,0 +1,12 @@
+package com.bia.weatherBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherBot {
+  public static void main(String[] args) {
+    SpringApplication.run(WeatherBot.class, args);
+  }
+
+}
